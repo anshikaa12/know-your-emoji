7f8ab528ac7c322c646eec0460cfe5c1fa34226a
Anshika Agarwal <65406794+anshikaa12@users.noreply.github.com>
2021-08-07 01:10:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  var emojiList = {
+    "😂": "laughter",
+    "😴": "sleepy",
+    "😒": "annoyed",
+    "🙄": "eyeroll",
+    "😏": "smirk",
+    "🥰": "loved"
+  };
+  var emojiWeKnow = Object.keys(emojiList);
+  var [inputEvent, setEvent] = useState("");
+
+  function changeF(event) {
+    inputEvent = event.target.value;
+    if (inputEvent in emojiList) {
+      inputEvent = emojiList[event.target.value];
+    } else if (inputEvent === "") {
+      inputEvent = "Translation will appeare here...";
+    } else {
+      inputEvent = "Sorry we don't know about this 😥";
+    }
+    return setEvent(inputEvent);
+  }
+  function emojiF(emoji) {
+    setEvent(emojiList[emoji]);
+  }
+  if (inputEvent === "") {
+    inputEvent = "Translation will appear here...";
+    setEvent(inputEvent);
+  }
+  return (
+    <div className="App">
+      <h1>😎 KNOW YOUR EMOJI 😎</h1>
+      <input onChange={changeF} placeholder="type your emoji here"></input>
+
+      <h3>{inputEvent}</h3>
+      <div>
+        {emojiWeKnow.map((emojis) => {
+          return (
+            <span
+              style={{ fontSize: "2rem", padding: " 0.5rem" }}
+              onClick={() => emojiF(emojis)}
+            >
+              {emojis}{" "}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
